f98b5b18811b89cbf4a2fafe00d871261755fac1
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-11-05 20:24:46 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqfkiqe.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running");
+});
+
+app.listen(port);
